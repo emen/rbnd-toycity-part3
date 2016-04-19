80ec4c354896cbc6df48d1915e1ff115ffc29c8e
Emen Zhao <emenzhaogq@gmail.com>
2016-04-18 23:18:01 -0700

    Complete product class

--- lib/product.rb
+++ lib/product.rb
@@ -1,27 +1,40 @@
 class Product
 
-  attr_reader :title
+  attr_reader :title, :price, :stock
 
   @@products = []
 
   def initialize(options = {})
-    @title = options[:title]
+    @title, @price, @stock = options.values_at(:title, :price, :stock)
     add_to_products
   end
 
+  def in_stock?
+    stock && stock > 0
+  end
+
   def self.all
-    @@products.clone
+    products.clone
+  end
+
+  def self.find_by_title(title)
+    products.find { |product| product.title == title }
+  end
+
+  def self.in_stock
+    products.select(&:in_stock?)
   end
 
   private
   def add_to_products
-    if products.any? { |product| product.title == title }
+    if self.class.products.any? { |product| product.title == title }
       raise DuplicateProductError, "#{title} already exists."
     end
-    products << self
+    self.class.products << self
   end
 
-  def products
+  private_class_method
+  def self.products
     @@products
   end
 
