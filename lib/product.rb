1bdda125d3f549e502efe831529ec2400fd69aa4
Emen Zhao <emenzhaogq@gmail.com>
2016-04-18 23:04:08 -0700

    Add check and raise error upon dup product.

--- lib/product.rb
+++ lib/product.rb
@@ -1,13 +1,28 @@
 class Product
 
+  attr_reader :title
+
   @@products = []
 
   def initialize(options = {})
-    @@products << self
+    @title = options[:title]
+    add_to_products
   end
 
   def self.all
     @@products.clone
+  end
+
+  private
+  def add_to_products
+    if products.any? { |product| product.title == title }
+      raise DuplicateProductError, "#{title} already exists."
+    end
+    products << self
+  end
+
+  def products
+    @@products
   end
 
 end
